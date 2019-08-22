b414f84d480171f54b135603d64faa57e35c7481
Nambi Sankaran <snambi@gmail.com>
2019-08-22 11:29:07 -0700

    first api /heros is working

--- src/hero.rs
+++ src/hero.rs
@@ -1,13 +1,12 @@
-
-
-use rocket::response::Responder;
+use rocket::http::{Status,ContentType};
+use rocket::response::{Responder, Body};
 use rocket::{Request, Response};
-use rocket::http::Status;
+use std::io::Cursor;
 //use rocket::request::Request;
 //use rocket::response::Result;
 
 #[derive(Serialize, Deserialize)]
-pub struct Hero{
+pub struct Hero {
     pub id: Option<i32>,
     pub name: String,
     pub identity: String,
@@ -15,27 +14,40 @@ pub struct Hero{
     pub age: i32,
 }
 
-impl Hero{
-
-    pub fn new( num:Option<i32>, name:String,
-            identity:String, hometown:String, age:i32 ) -> Hero {
-
-        Hero{ id:num,
-            name:name,
+impl Hero {
+    pub fn new(
+        num: Option<i32>,
+        name: String,
+        identity: String,
+        hometown: String,
+        age: i32,
+    ) -> Hero {
+        Hero {
+            id: num,
+            name: name,
             identity: identity,
             hometown: hometown,
-            age: 0 }
+            age: 0,
+        }
     }
 }
 
-impl <'r> Responder<'r> for Hero {
+impl<'r> Responder<'r> for Hero {
 
-    fn respond_to(self, request: &'r Request) -> Result<Response, Status> {
-        unimplemented!()
+    fn respond_to(self, request: &Request) -> Result<Response<'r>, Status> {
+
+        let stat = Status::Ok;
+
+        let response = Response::build()
+            .status(Status::ImATeapot)
+            .header(ContentType::Plain)
+            .raw_header("X-Teapot-Make", "Rocket")
+            .raw_header("X-Teapot-Model", "Utopia")
+            .raw_header_adjoin("X-Teapot-Model", "Series 1")
+            .sized_body(Cursor::new("Brewing the best coffee!"))
+            .finalize();
+
+        Result::Ok(response)
     }
 
-
-//    fn respond_to(self, request: &Request) -> response::Result<'r>{
-//
-//    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,62 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
-#[macro_use] extern crate rocket_contrib;
-#[macro_use] extern crate serde_derive;
+#[macro_use]
+extern crate rocket_contrib;
+#[macro_use]
+extern crate serde_derive;
 
-use rocket_contrib::json::{Json};
+use rocket_contrib::json::Json;
 
 mod hero;
 use hero::Hero;
 use serde_json::json;
 
-#[post("/", data = "<hero>" )]
-fn create( hero: Json<Hero>) -> Json<Hero>{
+
+#[post("/", data = "<hero>")]
+fn create(hero: Json<Hero>) -> Json<Hero> {
     //println!("Created called : {:?} ", hero);
     hero
 }
 
 #[get("/")]
 fn read() -> Hero {
-    let h = Hero::new(Some(33), String::from_str("John"),
-              String::from_str("human"),String::from_str("san jose"),
-              34);
+    let h = Hero::new(
+        Some(33),
+        String::from("John"),
+        String::from("human"),
+        String::from("san jose"),
+        34,
+    );
 
     h
 }
 
-#[put("/<id>", data="<hero>")]
-fn update( id:i32 , hero:Json<Hero> )-> Json<Hero> {
+#[put("/<id>", data = "<hero>")]
+fn update(id: i32, hero: Json<Hero>) -> Json<Hero> {
     hero
 }
 
 #[delete("/<id>")]
-fn delete( id:i32 ) -> Json<Hero>{
+fn delete(id: i32) -> Json<Hero> {
     //Json(json!({"status": "ok"}))
-    let hero = Hero::new(Some(33), String::from_str("John"),
-                         String::from_str("human"),String::from_str("san jose"),
-                         34);
-    Json( hero )
+    let hero = Hero::new(
+        Some(33),
+        String::from("John"),
+        String::from("human"),
+        String::from("san jose"),
+        34,
+    );
+    Json(hero)
 }
-
 
 fn main() {
     println!("Starting Rocket");
 
     rocket::ignite()
-        .mount("/hero", routes![create, update, delete] )
-        .mount("/heros", routes![read] )
+        .mount("/hero", routes![create, update, delete])
+        .mount("/heros", routes![read])
         .launch();
-
 }
